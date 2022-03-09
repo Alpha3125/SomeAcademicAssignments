4f32419f60004ab8b4fe58f037cb99f4032ac59a
Alpha3125 <girija4m@gmail.com>
2022-03-09 22:52:27 +0530

    Finished program 9.3.22

--- 2111050.py
+++ 2111050.py
@@ -1,8 +1,6 @@
-#merging same powers
-#removing power zeros
-#mult.
-#adding/removing comments
+#Algebraic Expression calculator
 
+#class containing attributes for univariable expressions
 class Univariate:
     def __init__(self):
         self.terms = 0
@@ -11,25 +9,42 @@ class Univariate:
         
     def start(self):
         self.terms = int(input('Enter no. of terms: '))
-        print('Enter the coefficients: ')
+        print('Enter the coefficient and degree of each term: ')
         for i in range (self.terms):
             self.coeff.append(int(input()))
-        print('Enter the degrees: ')
-        for i in range (self.terms):
             self.deg.append(int(input('x^')))
+            print('--')
+        self.arrange()
         self.disp()
 
     def disp(self):
-        polynomial = ''
-        for i in range(self.terms-1):
-            polynomial += str(self.coeff[i]) + 'x^' + str(self.deg[i]) + ' + '
-        polynomial += str(self.coeff[-1]) + 'x^' + str(self.deg[-1])
-        print('Polynomial: ',polynomial)
-        #print(self.terms, self.coeff, self.deg)
+        print('Polynomial: ', end = '')
+        for i in range(self.terms):
+            if self.coeff[i] == 0:
+                continue
+            elif self.coeff[i] < 0:
+                print(' - ', end = '')
+            elif self.coeff[i] > 0:
+                if i != 0:
+                    print(' + ', end = '')
+            if self.coeff[i]  > 0 and self.coeff[i] != 1:
+                print(self.coeff[i], end = '')
+            elif self.coeff[i]  < 0 and self.coeff[i] != -1:
+                print(-1*self.coeff[i], end = '')
+            if self.deg[i] == 0:
+                print('', end= '')
+                if self.coeff[i] == 1:
+                    print(self.coeff[i], end = '')
+                if self.coeff[i] == -1:
+                    print(-1*self.coeff[i], end = '')
+            elif self.deg[i] == 1:
+                print('x', end = '')
+            else:
+                print('x^', self.deg[i], end = '', sep='')
         print()
         
     def add(self, U):
-        for i in range(self.terms)
+        for i in range(self.terms):
             if self.deg[i] in U.deg:
                 self.coeff[i] += U.coeff[U.deg.index(self.deg[i])]
         for i in range(U.terms):
@@ -37,12 +52,11 @@ class Univariate:
                 self.terms +=1
                 self.deg.append(U.deg[i])
                 self.coeff.append(U.coeff[i])
-        self.disp()
 
     def multiply(self, U):
-        multdeg=[]
-	    multcoeff=[]
-	    multterms=self.terms*U.terms
+        multdeg = []
+        multcoeff = []
+        multterms = self.terms * U.terms
         for i in range(self.terms):
             for j in range(U.terms):
                 multcoeff.append(self.coeff[i]*U.coeff[j])
@@ -50,12 +64,37 @@ class Univariate:
         self.terms=multterms
         self.deg=multdeg
         self.coeff=multcoeff
-        self.disp()
 
     def arrange(self):
-        pass
-        
-class Polyvariate:
+        i = 0
+        while(i < self.terms-1):
+            j = i+1
+            while(j < self.terms):
+                if self.deg[i] ==  self.deg[j]:
+                    self.coeff[i] += self.coeff[j]
+                    del self.deg[j]
+                    del self.coeff[j]
+                    self.terms -= 1
+                    j -=1
+                j += 1
+            i += 1
+        i = 0
+        while(i < self.terms-1):
+            j = i+1
+            while(j < self.terms):
+                if self.deg[i] < self.deg[j]:
+                    temp=self.deg[i]
+                    self.deg[i]=self.deg[j]
+                    self.deg[j]=temp
+                    temp=self.coeff[i]
+                    self.coeff[i]=self.coeff[j]
+                    self.coeff[j]=temp
+                j += 1
+            i += 1
+
+
+#class containing attributes for multivariable expressions
+class Multivariate:
     def __init__(self):
         self.terms = 0
         self.variables = 0
@@ -65,31 +104,37 @@ class Polyvariate:
     def start(self):
         self.terms = int(input('Enter no. of terms: '))
         self.variables = int(input('Enter no. of variables: '))
-        print('Enter the coefficients: ')
+        print('Enter the coefficients and powers: ')
         for i in range (self.terms):
             self.coeff.append(int(input()))
-        print('Enter the degrees: ',self.terms)
-        for i in range (self.terms):
             powers=[]
             for j in range (self.variables):
                 powers.append(int(input(chr(112+j)+'^')))
             self.deg.append(powers)
             print('--')
+        self.arrange()
         self.disp()
 
     def disp(self):
-        polynomial = ''
+        polynomial = ' '
         for i in range(self.terms):
+            if self.coeff[i] == 0:
+                continue
             poly = ''
             for j in range(self.variables):
-                poly += '(' + str(chr(112+j)) + '^' + str(self.deg[i][j]) + ')'
-            if(i == self.terms-1):
+                if not self.deg[i][j] == 0:
+                    poly += '(' + str(chr(112+j)) + '^' + str(self.deg[i][j]) + ')'
+            if(i == 0):
                 polynomial += str(self.coeff[i]) + f'{poly}'
             else:
-                polynomial += str(self.coeff[i]) + f'{poly}' + ' + '
-        print('Polynomial: ',polynomial)
-        #print(self.terms, self.coeff, self.deg)
-        print()
+                polynomial += ' + ' + str(self.coeff[i]) + f'{poly}'
+        polynomial = polynomial.replace('+ -', '- ')
+        polynomial = polynomial.replace('^1)', ')')
+        polynomial = polynomial.replace(' 1(',' (')
+        if polynomial.strip() == '':
+            polynomial = '0'
+        print('Polynomial:',polynomial)
+        print('')
 
     def add(self, U):
         for i in range(self.terms):
@@ -100,16 +145,65 @@ class Polyvariate:
                 self.terms +=1
                 self.deg.append(U.deg[i])
                 self.coeff.append(U.coeff[i])
-        self.disp()
 
     def multiply(self, U):
-        pass
+        diff = U.variables - self.variables 
+        if diff > 0:
+            self.variables += diff
+            for i in range(self.terms):
+                for j in range(diff):
+                    self.deg[i].append(0)
+        elif diff < 0:
+            diff *= -1
+            U.variables += diff
+            for i in range(self.terms):
+                for j in range(diff):
+                    U.deg[i].append(0)
+        multdeg = []
+        multcoeff = []
+        multterms = self.terms * U.terms
+        for i in range(self.terms):
+            for j in range(U.terms):
+                multcoeff.append(self.coeff[i]*U.coeff[j])
+                D = []
+                for k in range(self.variables):
+                    D.append(self.deg[i][k] + U.deg[j][k])
+                multdeg.append(D)
+        self.terms=multterms
+        self.deg=multdeg
+        self.coeff=multcoeff
 
-    def arrange():
-        pass
+    def arrange(self):
+        i = 0
+        while(i < self.terms-1):
+            j = i+1
+            while(j < self.terms):
+                if self.deg[i] == self.deg[j]:
+                    self.coeff[i] += self.coeff[j]
+                    del self.deg[j]
+                    del self.coeff[j]
+                    self.terms -= 1
+                    j -= 1
+                j += 1
+            i += 1
+        i = 0
+        while(i < self.terms-1):
+            j = i+1
+            while(j < self.terms):
+                if sum(self.deg[i]) < sum(self.deg[j]):
+                    temp=self.deg[i]
+                    self.deg[i]=self.deg[j]
+                    self.deg[j]=temp
+                    temp=self.coeff[i]
+                    self.coeff[i]=self.coeff[j]
+                    self.coeff[j]=temp
+                j += 1
+            i +=1
 
 
-print('\n\t1.Univariate\n\t2.Multivariate')
+#main body
+print('\n---- ---- ---- ---- ---- ---- ---- ----\n')
+print('\t\tPOLYNOMIAL TYPE\n\t1.Univariate\n\t2.Multivariate')
 choice = int(input('Enter the type of polynomial you want to work with(1/2): '))
 if choice == 1:
     print('\n\t\tFIRST POLYNOMIAL')
@@ -118,24 +212,28 @@ if choice == 1:
     print('\t\tSECOND POLYNOMIAL')
     P2 = Univariate()
     P2.start()
-    P2.disp() 
 elif choice == 2:
     print('\n\t\tFIRST POLYNOMIAL')
-    P1 = Polyvariate()
+    P1 = Multivariate()
     P1.start()
     print('\t\tSECOND POLYNOMIAL')
-    P2 = Polyvariate()
-    P2.start()
-    P2.disp()    
+    P2 = Multivariate()
+    P2.start() 
 else:
     print('Invalid Input!!')
     exit()
-
 print('\n\t1.Addition\n\t2.Multiplication')
 choice = int(input('Enter your choice of operation to perform(1/2): '))
 if choice == 1:
     P1.add(P2)
+    P1.arrange()
+    print('\t\tOUTPUT')
+    P1.disp()
 elif choice == 2:
     P1.multiply(P2)
+    P1.arrange()
+    print('\n\t\tOUTPUT')
+    P1.disp()
 else:
     print('Invalid Input!!')
+print('\n---- ---- ---- ---- ---- ---- ---- ----\n')
